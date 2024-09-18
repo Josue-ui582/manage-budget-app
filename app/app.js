a1f0e160db9bf7edeb8bf0296fbec10bf5aafb00
JoshuaDev <hounhouijosue1@gmail.com>
2024-09-18 23:24:19 +0100

    update

--- app/app.js
+++ app/app.js
@@ -24,7 +24,9 @@ okButton.addEventListener("click", function () {
 });
 
 removeSolde.addEventListener("click", function () {
-    currentBallance.textContent = "0f";
+    currentBallanceValue = 0;
+    currentBallance.textContent = currentBallanceValue + "f";
+    newValueInput.value = "";
 })
 
 document.addEventListener("click", function (e) {
@@ -34,3 +36,39 @@ document.addEventListener("click", function (e) {
     }
 });
 
+document.getElementById('addRowBtn').addEventListener('click', function() {
+    
+    const tableBody = document.querySelector('#productTable tbody');
+    const newRow = document.createElement('tr');
+
+    
+    const productNameCell = document.createElement('td');
+    const productNameInput = document.createElement('input');
+    productNameInput.type = 'text';
+    productNameInput.placeholder = 'Nom de l\'article';
+    productNameInput.style.width = "100%";
+    productNameInput.style.fontSize = "20px";
+    productNameInput.style.padding = "5px";
+    productNameInput.style.outline = "none";
+    productNameInput.style.border = "none";
+    productNameCell.appendChild(productNameInput);
+
+    
+    const productPriceCell = document.createElement('td');
+    const productPriceInput = document.createElement('input');
+    productPriceInput.type = 'number';
+    productPriceInput.placeholder = 'Prix de l\'article';
+    productPriceInput.style.width = "100%";
+    productPriceInput.style.fontSize = "20px";
+    productPriceInput.style.padding = "5px";
+    productPriceInput.style.outline = "none";
+    productPriceInput.style.border = "none";
+    productPriceCell.appendChild(productPriceInput);
+
+    
+    newRow.appendChild(productNameCell);
+    newRow.appendChild(productPriceCell);
+
+    
+    tableBody.appendChild(newRow);
+});
