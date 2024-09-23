38a38af38f4d163c785e06e786ed482bd14f4dfc
JoshuaDev <hounhouijosue1@gmail.com>
2024-09-23 18:10:37 +0100

    update

--- front-end/javascript/investissement.js
+++ front-end/javascript/investissement.js
@@ -2,7 +2,12 @@ const add_3 = document.getElementById("add_3");
 const inputContainer_3 = document.getElementById("inputContainer_3");
 const newValueInput_3 = document.getElementById("newValue_3");
 const okButton_3 = document.getElementById("okButton_3");
+const historyContainer2 = document.getElementById("historyContainer2");
+const historyList2 = document.getElementById("historyList2");
+const historyIcon2 = document.getElementById("historyIcon2");
+
 let currentBallanceValue_3 = 0;
+let addedAmount2 = [];
 
 add_3.addEventListener("click", function () {
     inputContainer_3.style.display = "flex";
@@ -15,6 +20,9 @@ okButton_3.addEventListener("click", function () {
     if (!isNaN(newValue3) && newValue3 > 0) {
         currentBallanceValue += newValue3;
         currentBallance.textContent = currentBallanceValue + ' FCFA';
+
+        addedAmount2.push(newValue3);
+        upDateHistorique2();
     }
 
     newValueInput_3.value = "";
@@ -27,3 +35,56 @@ document.addEventListener("click", function (e) {
         inputContainer_3.style.display = "none"
     }
 });
+
+historyIcon2.addEventListener('click', function (e) {
+    e.stopPropagation();
+    toggleHistory2();
+});
+
+function toggleHistory2 () {
+    if (historyContainer2.style.display = "none") {
+        historyContainer2.style.display = "block";
+    } else {
+        historyContainer2.style.display = "none";
+    }
+};
+
+document.addEventListener('click', function (e) {
+    const isClickInside = historyContainer2.contains(e.target) || historyIcon2.contains(e.target);
+    
+    if (!isClickInside) {
+        historyContainer2.style.display = "none";
+    }
+});
+
+function upDateHistorique2() {
+    historyList2.innerHTML = '';
+
+    addedAmount2.forEach((amount, index) => {
+
+        const listItem2 = document.createElement('li');
+        listItem2.classList.add('history-item');
+
+        listItem2.innerHTML = `
+            <div class="historique__section">
+                <span class="amount">${amount} FCFA</span>
+                <button data-index="${index}">Supprimer</button>
+            </div>
+        `;
+        listItem2.querySelector('button').addEventListener('click', function () {
+            removeAmount2(index);
+        });
+
+        historyList2.appendChild(listItem2);
+    });
+};
+
+function removeAmount2 (index) {
+    const amountToRemove2 = addedAmount2[index];
+
+    currentBallanceValue -= amountToRemove2;
+    currentBallance.textContent = currentBallanceValue + ' FCFA';
+
+    addedAmount2.splice(index, 1);
+    upDateHistorique2();
+};
--- front-end/javascript/salaire.js
+++ front-end/javascript/salaire.js
@@ -5,13 +5,15 @@ const okButton_2 = document.getElementById("okButton_2");
 const historyContainer1 = document.getElementById("historyContainer1");
 const historyList1 = document.getElementById("historyList1");
 const historyIcon1 = document.getElementById("historyIcon1");
+let currentBallance1 = document.getElementById("currentBallance");
+
 let currentBallanceValue_2 = 0;
 let addedAmount1 = [];
 
 add_2.addEventListener("click", function () {
     inputContainer_2.style.display = "flex";
     newValueInput_2.focus();
-})
+});
 
 okButton_2.addEventListener("click", function () {
     const newValue2 = parseFloat(newValueInput_2.value);
@@ -21,12 +23,12 @@ okButton_2.addEventListener("click", function () {
         currentBallance.textContent = currentBallanceValue + ' FCFA';
 
         addedAmount1.push(newValue2);
-        upDateHistorique();
+        upDateHistorique1();
     }
 
     newValueInput_2.value = "";
     inputContainer_2.style.display = "none"
-})
+});
 
 document.addEventListener("click", function (e) {
     const isClickInside_2 = add_2.contains(e.target) || inputContainer_2.contains(e.target);
@@ -37,6 +39,53 @@ document.addEventListener("click", function (e) {
 
 historyIcon1.addEventListener('click', function (e) {
     e.stopPropagation();
-    toggleHistory();
+    toggleHistory1();
 });
 
+function toggleHistory1() {
+    if (historyContainer1.style.display = "none") {
+        historyContainer1.style.display = "block";
+    }else{
+        historyContainer1.style.display = "none";
+    }
+};
+
+document.addEventListener('click', function (e) {
+    const isClickInside = historyContainer1.contains(e.target) || historyIcon1.contains(e.target);
+    
+    if (!isClickInside) {
+        historyContainer1.style.display = "none";
+    }
+});
+
+function upDateHistorique1() {
+    historyList1.innerHTML = '';
+
+    addedAmount1.forEach((amount, index) => {
+
+        const listItem1 = document.createElement('li');
+        listItem1.classList.add('history-item');
+
+        listItem1.innerHTML = `
+            <div class="historique__section">
+                <span class="amount">${amount} FCFA</span>
+                <button data-index="${index}">Supprimer</button>
+            </div>
+        `;
+        listItem1.querySelector('button').addEventListener('click', function () {
+            removeAmount1(index);
+        });
+
+        historyList1.appendChild(listItem1);
+    });
+};
+
+function removeAmount1 (index) {
+    const amountToRemove1 = addedAmount1[index];
+
+    currentBallanceValue -= amountToRemove1;
+    currentBallance.textContent = currentBallanceValue + ' FCFA';
+    
+    addedAmount1.splice(index, 1);
+    upDateHistorique1();
+};
